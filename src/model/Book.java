57913e073d4fe1b3d52283a4dd2cc55b623630f8
Maycool Vinicius <maycoolvinicius_nh_@hotmail.com>
2026-07-21 00:07:03 -0300

    wip: add Book and start Loan (Loan still not compile, missing LoanStatus)

---
+++ src/model/Book.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Book{
+	
+	private int id;
+	private String title;
+	private String author;
+	private String isbn;
+	
+	public Book(int id, String title, String author, String isbn){
+	this.id = id;
+	this.title = title;
+	this.author = author;
+	this.isbn = isbn;
+	}
+	
+	public int getId(){
+	return id;
+	}
+	public String getTitle(){
+	return title;
+	}
+	public String getAuthor(){
+	return author;
+	}
+	public String getIsbn(){
+	return isbn;
+	}
+	
+	public void setTitle(String title){
+	this.title = title;
+	}
+	public void setAuthor(String author){
+	this.author = author;
+	}
+	public void setIsbn(String isbn){
+	this.isbn = isbn;
+	}
+}
---
+++ src/model/Loan.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Loan{
+
+	private int id;
+	private LocalDate loanAt;
+	private LocalDate dueDate;
+	private LoanStatus status;
+	private Users user;
+	private Book book;
+
+}
